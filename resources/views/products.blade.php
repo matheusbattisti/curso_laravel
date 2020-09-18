2070bbfc3c91f61db17d706168739ea3e198c6c9
Matheus Battisti <matheusbudkewicz@gmail.com>
2020-09-17 21:59:50 -0300

    arquivos estáticos

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,15 @@
-<h1>Tela de produtos</h1>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+  <body>
+    <h1>Tela de produtos</h1>
+  </body>
+</html>
