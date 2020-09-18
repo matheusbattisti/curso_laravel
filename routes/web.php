1cbe60e256e24738105f7e1de19beffcf7191c4b
Matheus Battisti <matheusbudkewicz@gmail.com>
2020-09-17 21:47:41 -0300

    Aulas de Blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Matheus";
+    $idade = 29;
+
+    $arr = [10,20,30,40,50];
+
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view('welcome', 
+        [
+            'nome' => $nome, 
+            'idade2' => $idade, 
+            'profissao' => "Programador",
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]);
 });
 
 Route::get('/contact', function () {
