79768f788ef1ae1e7137715225330b8a06862237
Matheus Battisti <matheusbudkewicz@gmail.com>
2020-09-21 21:49:09 -0300

    flash message

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -32,7 +32,7 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
 
     }
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,16 @@
           </div>
         </nav>
       </header>
-      @yield('content')
+      <main>
+        <div class="container-fluid">
+          <div class="row">
+            @if(session('msg'))
+              <p class="msg">{{ session('msg') }}</p>
+            @endif
+            @yield('content')
+          </div>
+        </div>
+      </main>
       <footer>
         <p>HDC Events &copy; 2020</p>
       </footer>
