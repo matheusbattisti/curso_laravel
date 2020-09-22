e67df94ac1e6732306d144af98c9be162afe51ad
Matheus Battisti <matheusbudkewicz@gmail.com>
2020-09-22 20:58:20 -0300

    busca

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,9 +11,19 @@ class EventController extends Controller
     
     public function index() {
 
-        $events = Event::all();
+        $search = request('search');
+
+        if($search) {
+
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+
+        } else {
+            $events = Event::all();
+        }        
     
-        return view('welcome',['events' => $events]);
+        return view('welcome',['events' => $events, 'search' => $search]);
 
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,17 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method="GET">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
+    @if($search)
+    <h2>Buscando por: {{ $search }}</h2>
+    @else
     <h2>Próximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
@@ -25,7 +29,9 @@
             </div>
         </div>
         @endforeach
-        @if(count($events) == 0)
+        @if(count($events) == 0 && $search)
+            <p>Não foi possível encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos</a></p>
+        @elseif(count($events) == 0)
             <p>Não há eventos disponíveis</p>
         @endif
     </div>
